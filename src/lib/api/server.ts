fba1447fd95b98f922bfd4657007bc34f4d65314
jonathanferreras <jonathanferreras@icloud.com>
2025-07-03 10:40:39 -0400

    added rest api to send mqtt publish

--- src/lib/api/server.ts
+++ src/lib/api/server.ts
@@ -1,9 +1,33 @@
 import Fastify, { FastifyInstance } from 'fastify';
+import cors from '@fastify/cors';
+import { mqttClient } from '../mqtt/mqttClient';
 
 const app: FastifyInstance = Fastify({ logger: true });
 
+app.register(cors, {
+  origin: '*',
+});
+
 app.get('/', async () => {
   return { message: 'Hello from Fastify' };
+});
+
+app.post('/publish', async (request, response) => {
+  try {
+    const { message } = request.body as {
+      message: string;
+    };
+
+    if (!message) {
+      return response.status(400).send({ error: 'Message is required!' });
+    }
+
+    mqttClient.publish(process.env.MQTT_PUB_TOPIC!, message);
+    return response.send({ success: true });
+  } catch (error) {
+    request.log.error(error);
+    return response.status(500).send({ error: 'Failed to publish to MQTT' });
+  }
 });
 
 async function start(host: string, port: number) {
